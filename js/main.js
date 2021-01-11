3fe2b8fb926df2d1c028d36ebef57bfd5210f6d4
Alle <agileontheweb@gmail.com>
2021-01-12 00:08:20 +0100

    Show showProgressBarRemaingPieces on visible in the page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
   countDown();
+  showProgressBarRemaingPieces();
+
+  function showProgressBarRemaingPieces(){
+    var observer = new IntersectionObserver(function(entries) {
+  	if(entries[0].isIntersecting === true)
+  		console.log('Element is fully visible in screen');
+      animateProgressBar();
+    }, { threshold: [0] });
+
+    observer.observe(document.querySelector(".progress-short"));
+
+    function animateProgressBar(){
+      flag=true;
+      remaing_pieces = $(".progress-bar-animate").data("remaing-pieces");
+
+      $({someValue: 100}).animate({someValue: remaing_pieces}, {
+      	duration: 3000,
+      	easing:'swing',
+      	step: function() {
+      	   $('.progress-short-number').text(Math.ceil(this.someValue));
+           $(".progress-bar-animate").width(this.someValue + '%');
+      	}
+      });
+    }
+  }
+
+
 
   var myCarousel = document.querySelector('#myCarousel')
   var carousel = new bootstrap.Carousel(myCarousel, {
--- shared/progress-short.php
+++ shared/progress-short.php
@@ -1,12 +1,15 @@
-<div class="relative pt-1  text-white p-3 my-3 text-red-600">
-
+<div class="relative pt-1  text-white p-3 my-3 text-red-600 progress-short">
   <div class="mt-1 h-2 bg-gray-200">
-    <div class="mt-2 bg-red-600 py-1 text-center h-2" style="width:<?php remainingPieces();?>%"></div>
+    <div id="progress-bar-animate" class="progress-bar-animate mt-2 bg-red-600 py-1 text-center h-2" style="width:100%" data-remaing-pieces="<?php remainingPieces(); ?>"></div>
   </div>
+
   <div class="mx-auto text-center">
     <p class="font-semibold text-white text-center text-red-600 inline-block">
       Disponibilità giornaliera
     </p>
-    <div class="border rounded-md bg-red-600 mt-2 text-red-100 text-sm p-2 inline-block px-2"><?php echo remainingPieces(); ?> pezzi</div>
+    <div class="progress-short-number border rounded-md bg-red-600 mt-2 text-red-100 text-sm p-2 inline-block px-2">
+      <?php echo remainingPieces(); ?>
+    </div>
+    pezzi
   </div>
 </div>
