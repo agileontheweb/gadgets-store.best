650b511ac816baaee149bf44edc22d86fadcc755
Alle <agileontheweb@gmail.com>
2021-01-20 17:22:58 +0100

    Add link on Card

--- shared/_perlacasa.php
+++ shared/_perlacasa.php
@@ -24,24 +24,26 @@
     <div class="p-2">
       <div class="bg-white rounded-lg shadow-xl">
         <div class="p-12">
-          <div>
-           <img src="<?php echo $projectPath ?><?php echo $projectPathImg ?><?php echo $list[$i]->image ?>" alt="<?php echo $list[$i]->name ?>" width="300" height="300">
-          </div>
-          <div class="text-center pt-3">
-            <?php if($list[$i]->offer){?>
-              <span class="<?php colorBadge($list[$i]->offer) ?> inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none rounded-full">
-                <?php echo $list[$i]->offer ?>
-              </span>
-            <?php } ?>
-            <span class="block text-xs mt-2"><?php echo $list[$i]->prezzo ?></span>
-          </div>
-          <div class="mt-8 text-center">
-            <h2 class="font-bold text-lg text-gray-700 mb-1 uppercase"><?php echo $list[$i]->name ?></h2>
-            <p class="text-gray-600"><?php echo $list[$i]->description ?></p>
-            <a href="<?php echo $value ?>" class="block mt-6 bg-gray-500 hover:bg-gray-400 text-white rounded-full px-12 py-3 shadow-xl focus:outline-none" <?php if($target == true){?> target="_blank" rel="nofollow" <?php }?>>
-              Vedi
-            </a>
-          </div>
+          <a href="<?php echo $value ?>" <?php if($target == true){?> target="_blank" rel="nofollow" <?php }?>>
+            <div>
+             <img src="<?php echo $projectPath ?><?php echo $projectPathImg ?><?php echo $list[$i]->image ?>" alt="<?php echo $list[$i]->name ?>" width="300" height="300">
+            </div>
+            <div class="text-center pt-3">
+              <?php if($list[$i]->offer){?>
+                <span class="<?php colorBadge($list[$i]->offer) ?> inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none rounded-full">
+                  <?php echo $list[$i]->offer ?>
+                </span>
+              <?php } ?>
+              <span class="block text-xs mt-2"><?php echo $list[$i]->prezzo ?></span>
+            </div>
+            <div class="mt-8 text-center">
+              <h2 class="font-bold text-lg text-gray-700 mb-1 uppercase"><?php echo $list[$i]->name ?></h2>
+              <p class="text-gray-600"><?php echo $list[$i]->description ?></p>
+              <p class="block mt-6 bg-gray-500 hover:bg-gray-400 text-white rounded-full px-12 py-3 shadow-xl focus:outline-none">
+                Vedi
+              </p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
@@ -49,22 +51,24 @@
     <div class="p-2">
       <div class="bg-black text-white rounded-lg shadow-xl">
         <div class="p-12">
-          <div>
-           <img src="<?php echo $projectPath ?>/img/logo.png" alt="">
-          </div>
-          <div class="mt-8 text-center">
-            <h2 class="font-bold text-lg mb-1 uppercase">Scopri altri prodotti in offerta</h2>
-            <a href="prodotti-casa.php" class="block mt-6 text-black bg-white hover:bg-black border-4 hover:border-4 hover:border-red-400 hover:text-white rounded-full px-12 py-3 shadow-xl focus:outline-none">
-              Vai
-            </a>
-          </div>
+          <a href="prodotti-casa.php">
+            <div>
+             <img src="<?php echo $projectPath ?>/img/logo.png" alt="">
+            </div>
+            <div class="mt-8 text-center">
+              <h2 class="font-bold text-lg mb-1 uppercase">Scopri altri prodotti in offerta</h2>
+              <p class="block mt-6 text-black bg-white hover:bg-black border-4 hover:border-4 hover:border-red-400 hover:text-white rounded-full px-12 py-3 shadow-xl focus:outline-none">
+                Vai
+              </p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
   </div>
   <div class="text-center py-12">
-    <a href="prodotti-casa.php" class="border-2  p-3 rounded text-gray-500 border-gray-500 hover:bg-gray-500 hover:text-white">
-    Vedi tutti
-  </a>
+    <a href="prodotti-casa.php" class="inline-block border-2  p-3 rounded text-gray-500 border-gray-500 hover:bg-gray-500 hover:text-white">
+      Vedi tutti
+    </a>
   </div>
 </div>
--- shared/_tecnologia.php
+++ shared/_tecnologia.php
@@ -25,52 +25,57 @@
     <div class="p-2">
       <div class="bg-white rounded-lg shadow-xl">
         <div class="p-12">
-          <div>
-           <img src="<?php echo $projectPath ?><?php echo $projectPathImg ?><?php echo $list[$i]->image ?>" alt="<?php echo $list[$i]->name ?>" width="300" height="300">
-          </div>
+          <a href="<?php echo $value ?>" class="" <?php if($target == true){?> target="_blank" rel="nofollow" <?php }?>>
+            <div>
+             <img src="<?php echo $projectPath ?><?php echo $projectPathImg ?><?php echo $list[$i]->image ?>" alt="<?php echo $list[$i]->name ?>" width="300" height="300">
+            </div>
 
-          <div class="text-center pt-3">
-            <?php if($list[$i]->offer){?>
-            <span class="<?php colorBadge($list[$i]->offer) ?> inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none rounded-full">
-              <?php echo $list[$i]->offer ?>
-            </span>
-            <span class="block text-xs mt-2"><?php echo $list[$i]->prezzo ?></span>
-            <?php } ?>
-          </div>
+            <div class="text-center pt-3">
+              <?php if($list[$i]->offer){?>
+              <span class="<?php colorBadge($list[$i]->offer) ?> inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none rounded-full">
+                <?php echo $list[$i]->offer ?>
+              </span>
+              <span class="block text-xs mt-2"><?php echo $list[$i]->prezzo ?></span>
+              <?php } ?>
+            </div>
 
-          <div class="mt-8 text-center">
-            <h2 class="font-bold text-lg text-gray-700 mb-1 uppercase"><?php echo $list[$i]->name ?></h2>
-            <p class="text-gray-600"><?php echo $list[$i]->description ?></p>
-            <a href="<?php echo $value ?>" class="block mt-6 bg-blue-500 hover:bg-blue-400 text-white rounded-full px-12 py-3 shadow-xl focus:outline-none" <?php if($target == true){?> target="_blank" rel="nofollow" <?php }?>>
-              Vedi
-            </a>
+            <div class="mt-8 text-center">
+              <h2 class="font-bold text-lg text-gray-700 mb-1 uppercase"><?php echo $list[$i]->name ?></h2>
+              <p class="text-gray-600"><?php echo $list[$i]->description ?></p>
+              <p class="block mt-6 bg-blue-500 hover:bg-blue-400 text-white rounded-full px-12 py-3 shadow-xl focus:outline-none" <?php if($target == true){?> target="_blank" rel="nofollow" <?php }?>>
+                Vedi
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
+
     <?php } } ?>
     <div class="p-2">
       <div class="bg-black text-white rounded-lg shadow-xl">
-        <div class="p-12">
-          <div>
-           <img src="<?php echo $projectPath ?>/img/logo.png" alt="">
+        <a href="prodotti-tecnologia.php">
+          <div class="p-12">
+            <div>
+             <img src="<?php echo $projectPath ?>/img/logo.png" alt="">
+            </div>
+            <div class="mt-8 text-center">
+              <h2 class="font-bold text-lg mb-1 uppercase">Scopri altri prodotti in offerta</h2>
+              <p class="block mt-6 text-black bg-white hover:bg-black border-4 hover:border-4 hover:border-red-400 hover:text-white rounded-full px-12 py-3 shadow-xl focus:outline-none">
+                Vai
+              </p>
+            </div>
           </div>
-          <div class="mt-8 text-center">
-            <h2 class="font-bold text-lg mb-1 uppercase">Scopri altri prodotti in offerta</h2>
-            <a href="prodotti-tecnologia.php" class="block mt-6 text-black bg-white hover:bg-black border-4 hover:border-4 hover:border-red-400 hover:text-white rounded-full px-12 py-3 shadow-xl focus:outline-none">
-              Vai
-            </a>
-          </div>
-        </div>
+        </a>
       </div>
     </div>
   </div>
 
 
   <div class="text-center py-12">
-    <a href="prodotti-tecnologia.php" class="border-2  p-3 rounded text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white">
-    Vedi tutti
-  </a>
+    <a href="prodotti-tecnologia.php" class="block-inline border-2  p-3 rounded text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white">
+      Vedi tutti
+    </a>
 
   </div>
 </div>
