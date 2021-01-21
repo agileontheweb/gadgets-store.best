4b25029cdcc63407f92dd57aaa9c73f85f4069f0
Alle <agileontheweb@gmail.com>
2021-01-21 18:43:55 +0100

    Fix HotJar

--- shared/px-hotjar.php
+++ shared/px-hotjar.php
@@ -1,14 +1,11 @@
-<?php
-if( !in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) { ?>
-  <!-- Hotjar Tracking Code for www.gadgets-store.best -->
-  <script>
-      (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:<?php echo $px_hotjar?> ,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-  </script>
-<?php } ?>
+<!-- Hotjar Tracking Code for www.gadgets-store.best -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:<?php echo $px_hotjar?> ,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
