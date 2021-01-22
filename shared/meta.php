5b7753525ef95566d98e2e433a15afd00505b35a
Alle <agileontheweb@gmail.com>
2021-01-22 12:34:26 +0100

    Add dn-prefetch hotjar

--- shared/meta.php
+++ shared/meta.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <link rel="dns-prefetch" href="//static.hotjar.com">
+<link rel="dns-prefetch" href="//in.hotjar.com">
 <link rel="dns-prefetch" href="//www.google-analytics.com">
 <link rel="dns-prefetch" href="//www.facebook.com">
 
