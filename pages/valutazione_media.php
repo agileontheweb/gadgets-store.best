082b6217c724a695c05acbc77d6d3772e754aa35
Alle <agileontheweb@gmail.com>
2021-01-22 12:39:20 +0100

    Remove animation to valutazione media

--- pages/valutazione_media.php
+++ pages/valutazione_media.php
@@ -1,5 +1,5 @@
-<div class="text-center py-3 <?php addMarginTop() ?> animate__animated animate__tada">
-    <strong>Valutazione Media: <?php echo $average_rating ?></strong>
+<div class="text-center py-3 <?php addMarginTop() ?>">
+  <strong>Valutazione Media: <?php echo $average_rating ?></strong>
   <i class="fas fa-star fa-1x text-yellow-500"></i>
   <i class="fas fa-star fa-1x text-yellow-500"></i>
   <i class="fas fa-star fa-1x text-yellow-500"></i>
