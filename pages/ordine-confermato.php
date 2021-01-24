11526e470635fbd201462989037764d94463a02e
Alle <agileontheweb@gmail.com>
2021-01-24 19:22:20 +0100

    fix

--- pages/ordine-confermato.php
+++ pages/ordine-confermato.php
@@ -18,7 +18,7 @@
     <?php include('../shared/meta.php'); ?>
   </head>
   <body>
-    <?php if(! in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) {?>
+    <?php if(isset($env)=="prod") {?>
       <script>
         fbq('track', 'Purchase ' , {currency: "EUR", value: <?php echo $price; ?>});
       </script>
